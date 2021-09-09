05a81a897966aa7ab87d9b550ccca8fa897b8f10
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-09-08 23:24:47 -0500

    add: rutas al enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+};
+
+export const apiV1 = {
+  login: 'https://matest.xyz/maxoutapi/api/login/authenticate',
+  register: 'https://matest.xyz/maxoutapi/api/login/register_user_test',
+  blogs: 'https://matest.xyz/maxoutapi/api/blogs/list_blogs',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+};
+
+export const apiV1 = {
+  login: 'https://matest.xyz/maxoutapi/api/login/authenticate',
+  register: 'https://matest.xyz/maxoutapi/api/login/register_user_test',
+  blogs: 'https://matest.xyz/maxoutapi/api/blogs/list_blogs',
 };
 
 /*
